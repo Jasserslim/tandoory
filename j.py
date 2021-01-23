d2688d8fce842783d2161e95636fa04a2c3d8e28
Jasserslim <57301026+Jasserslim@users.noreply.github.com>
2021-01-23 16:59:16 +0100

    Update j.py

--- j.py
+++ j.py
@@ -26,8 +26,17 @@ def parse_arg_from_requests(arg, **kwargs):
 def parse_params(param):
     return json.dumps([x.strip() for x in param.split(",")])
 
-def model () :
-    vgg_model = VGG16(include_top=False,
+def VGGmodel () :
+    from keras.applications.vgg16 import VGG16
+    from tensorflow.keras.applications.vgg16 import VGG16
+    from keras.models import Model
+    from keras.layers import MaxPooling2D
+    from keras.layers import GlobalAveragePooling2D, Dense,BatchNormalization
+    from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, InputLayer
+    from keras.models import Sequential
+    from keras import optimizers
+    vgg_model = VGG16(weights='imagenet',
+                                include_top=False,
                                 input_shape=(224, 224, 3))
 
     x4 = vgg_model.output  
@@ -37,16 +46,18 @@ def model () :
     x4 = Dense(512, activation ='relu')(x4) 
     x4 = BatchNormalization()(x4) 
     x4 = Dropout(0.5)(x4) 
-     
+    
     x4 = Dense(2, activation ='softmax')(x4)  
-    vgg_model = Model(vgg_model.input, x4)
+    vgg_model = Model(vgg_model.input, x4)  
 
+    vgg_model.compile(loss='categorical_crossentropy',
+                        optimizer=optimizers.RMSprop(lr=2e-5),
+                        metrics=['accuracy'])
 
-    vgg_model.load_weights('j.hdf5')
+    vgg_model.load_weights("./weights.vgg_model.best.inc.male.hdf5")
 
     return(vgg_model)
 
-model = model()
 app = Flask(__name__)
 api = Api(app)
 
@@ -55,14 +66,14 @@ class predict(Resource) :
         keys =  parse_arg_from_requests('key')
         keys='./'+keys
         print(keys)
-        image = plt.imread(keys)
+        image = plt.imread(keys,0)
         #Resizing and reshaping to keep the ratio.
-        resized = resize(image, (1,224,224,3))
-        vect = np.asarray(resized, dtype="uint8")
+        my_image_resized = resize(image, (224,224,3))
+        model = VGGmodel()
         print('start')
-        my_prediction = model.predict(vect)
+        probabilities = model.predict(np.array( [my_image_resized,] ))
         print('end')
-        index = my_prediction[0]
+        index = probabilities
         index = index.tolist()
         return Response(json.dumps(index),status=200,mimetype='application/json')
 
